caa115b0d5f14499f06d72593455aba1a223f8b9
Vladislav Peltikhin <peltikhin.vladik@gmail.com>
2024-11-05 15:40:17 +0300

    392. Is Subsequence

---
+++ src/main/java/org/example/Problem392IsSubsequence.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Problem392IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int sIndex = 0;
+        int tIndex = 0;
+        while (sIndex < s.length() && tIndex < t.length()) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+            }
+            tIndex++;
+        }
+        return sIndex == s.length();
+    }
+}
