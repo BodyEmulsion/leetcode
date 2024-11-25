cb56a239b9bace3c02b1c68ab2470ae976f6b174
Vladislav Peltikhin <peltikhin.vladik@gmail.com>
2024-11-25 20:58:42 +0300

    328. Odd Even Linked List

---
+++ src/main/java/org/example/Problem328OddEvenLinkedList.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Problem328OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenStart = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenStart;
+        return head;
+    }
+}
