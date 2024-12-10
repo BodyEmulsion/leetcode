c65dee1f810ef876b808b5c0a683c7ca998fdafa
Vladislav Peltikhin <peltikhin.vladik@gmail.com>
2024-12-10 16:06:12 +0300

    700. Search in a Binary Search Tree

---
+++ src/main/java/org/example/Problem700SearchInABinarySearchTree.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Problem700SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (val == root.val) {
+            return root;
+        }
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
